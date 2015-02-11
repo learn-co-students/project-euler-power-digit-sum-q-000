5f2e50192aaa2c0a9bd58454ceea3a2dd51bdc7c
Rose Weixel <rose.weixel@gmail.com>
2015-02-11 15:18:55 -0500

    encrypt answer

--- spec/project_euler_power_digit_sum_spec.rb
+++ spec/project_euler_power_digit_sum_spec.rb
@@ -1,5 +1,7 @@
 describe "#power_digit_sum" do
+  let(:encrypted_answer){"MTM2Ng==\n"}
+
   it "takes a base (x) and exponent (n) as arguments and returns the sum of the digits of x raised to the power of n" do
-    expect(power_digit_sum(2, 1000)).to eq(1366)
+    expect(Base64.encode64(power_digit_sum(2, 1000).to_s)).to eq(encrypted_answer)
   end
 end
