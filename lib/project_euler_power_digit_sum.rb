91543768673d08ad5f6edf8ee468e15088812a58
cbruen <bruen.chris@gmail.com>
2015-12-17 03:30:59 +0000

    Done.

--- lib/project_euler_power_digit_sum.rb
+++ lib/project_euler_power_digit_sum.rb
@@ -1,1 +1,9 @@
 # code your solution here
+
+def power_digit_sum(one, two)
+    sep = one
+    (two-1).times do
+    sep = sep * one
+    end
+    sep = sep.to_s.split("").map { |x| x.to_i}.inject(:+)
+end
