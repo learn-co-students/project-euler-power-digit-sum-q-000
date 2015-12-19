003411c622d6ce4aae1da5aab975f4bfcc7a918f
azheng249 <andy.zheng249@gmail.com>
2015-12-19 21:44:11 +0000

    Done.

--- lib/project_euler_power_digit_sum.rb
+++ lib/project_euler_power_digit_sum.rb
@@ -1,1 +1,15 @@
 # code your solution here
+
+def sum_of_digits(number)
+  number = number.to_s
+  sum = 0
+  for i in 0...number.length
+    sum += number[i].to_i
+  end
+  return sum
+end
+
+def power_digit_sum(base, exponent)
+  power = base ** exponent
+  return sum_of_digits(power)
+end
