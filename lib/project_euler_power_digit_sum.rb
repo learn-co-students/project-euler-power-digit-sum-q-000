d9fab3ef29304dffab99b88756e58c8b1e187988
Juan Ruiz <juankboards@hotmail.com>
2015-12-30 02:32:17 +0000

    Done.

--- lib/project_euler_power_digit_sum.rb
+++ lib/project_euler_power_digit_sum.rb
@@ -1,1 +1,5 @@
-# code your solution here
+def power_digit_sum(base, exponent)
+  number = base**exponent
+  digits = number.to_s.split("").collect {|digit| digit.to_i}
+  power_digit_sum = digits.reduce(:+)
+end
