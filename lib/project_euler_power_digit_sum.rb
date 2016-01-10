09d31917b4a90e820d21ad21a4ef5b299c57bdfb
alexbarron <alexbarron@gmail.com>
2016-01-10 11:46:54 -0800

    Done.

--- lib/project_euler_power_digit_sum.rb
+++ lib/project_euler_power_digit_sum.rb
@@ -1,1 +1,10 @@
 # code your solution here
+def power_digit_sum(base, exp)
+  i = base ** exp
+  digits = i.to_s.chars.map(&:to_i)
+  sum = 0
+  digits.each do |digit|
+    sum += digit
+  end
+  return sum
+end
