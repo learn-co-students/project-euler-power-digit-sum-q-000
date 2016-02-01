2af92804a34fb94a5caa80944a1ad9e4d13a90f9
jes708 <jonschwarz@nyu.edu>
2016-01-31 23:07:53 -0500

    Done.

--- lib/project_euler_power_digit_sum.rb
+++ lib/project_euler_power_digit_sum.rb
@@ -1,1 +1,11 @@
 # code your solution here
+def power_digit_sum(x, n)
+
+  string = (x ** n).to_s 
+  arr = string.split(//)
+
+  result = 0
+  arr.each {|num| result += num.to_i}
+  
+  result
+end
