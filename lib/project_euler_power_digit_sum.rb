2ee6afffa514623e5a09698f07cf241108388a0a
einarstensson <einar.stensson@gmail.com>
2016-03-22 22:03:01 -0400

    Done.

--- lib/project_euler_power_digit_sum.rb
+++ lib/project_euler_power_digit_sum.rb
@@ -1,1 +1,10 @@
-# code your solution here
+def power_digit_sum(number, exponent)
+  result = number ** exponent
+  sum = 0
+
+  result.to_s.each_char do |digit|
+    sum += digit.to_i
+  end
+
+  sum
+end
