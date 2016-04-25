b213917f9f11c8a910040ff6325f66975685be3c
rikari0813 <christiane.maico@gmail.com>
2016-04-25 14:07:48 +0000

    Done.

--- lib/project_euler_power_digit_sum.rb
+++ lib/project_euler_power_digit_sum.rb
@@ -1,1 +1,9 @@
-# code your solution here
+def power_digit_sum(base,exponent)
+  newP = base ** exponent
+  arrayP = newP.to_s.chars.map(&:to_i)
+  sumT = 0
+  arrayP.each {|n|
+    sumT = sumT + n
+  }
+  sumT
+end
