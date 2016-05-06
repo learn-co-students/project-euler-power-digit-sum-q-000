4806392175eefce3d6c3c15728693890928c88e5
Yechiel Kalmenson <chilikkal@gmail.com>
2016-05-05 21:09:19 -0400

    Done.

--- lib/project_euler_power_digit_sum.rb
+++ lib/project_euler_power_digit_sum.rb
@@ -1,1 +1,10 @@
 # code your solution here
+def power_digit_sum(base, exponent)
+  power = base ** exponent
+  sum = 0
+  power.to_s.each_char do |n|
+    n = n.to_i
+    sum = n + sum
+  end
+  sum
+end
