012a7387e135f8c933b27d84eb719f7afd282be6
uu <giladbarkan@live.com>
2016-06-08 21:41:26 -0400

    passed tests

--- lib/project_euler_power_digit_sum.rb
+++ lib/project_euler_power_digit_sum.rb
@@ -1,1 +1,15 @@
 # code your solution here
+def digit_sum(n)
+  sum = 0
+  
+  while n > 0
+    sum += n % 10
+    n /= 10
+  end
+  
+  sum
+end
+
+def power_digit_sum(base, exponent)
+  digit_sum(base**exponent)
+end
