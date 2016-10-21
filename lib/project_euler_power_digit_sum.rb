fa41a83f8e55d0bb0fc65271868849c5fc5ec7cf
Sean Koo <koo.sean@gmail.com>
2016-10-21 03:08:07 +0000

    Done.

--- lib/project_euler_power_digit_sum.rb
+++ lib/project_euler_power_digit_sum.rb
@@ -1,1 +1,12 @@
 # code your solution here
+def power_digit_sum(base,exp)
+  num = base**exp
+#  puts "num #{num}"
+  stg = num.to_s
+#  puts "stg #{stg}"
+  answer = 0
+  for i in 0..stg.length
+    answer +=stg[i].to_i
+  end
+  return answer
+end
